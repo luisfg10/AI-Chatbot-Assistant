0b1a7216ead0c18777c9a811a35efccac645e91d
luisfg10 <luisf.gonzalezv@yahoo.com>
2026-07-04 21:00:47 -0500

    add math tool with guardrails

--- src/chatbot/tools/__init__.py
+++ src/chatbot/tools/__init__.py
@@ -1,11 +1,13 @@
 """Entrypoint for the tools module for the agent."""
 from src.chatbot.tools.builder import build_tools
 from src.chatbot.tools.definitions import (
+    evaluate_math_expression,
     get_current_date
 )
 
 # Build list of available tools
 tools = [
+    evaluate_math_expression,
     get_current_date
 ]
 
